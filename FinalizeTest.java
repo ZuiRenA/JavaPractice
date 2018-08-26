59c199d5e563285d78b6760de5b03af6fb7eb53a
ZuiRen <13405665741@163.com>
2018-08-26 17:59:48 +0800

    finalize()方法练习

--- FinalizeTest.java
+++ FinalizeTest.java
@@ -1,11 +1,36 @@
 public class FinalizeTest {
     public static void main(String[] args){
+        /*FinalizeTry finalizeTry = new FinalizeTry();
+        finalizeTry = null;
+        System.gc();
+        System.runFinalization();
+        */
+        new Tank();
+        System.gc();
+        System.runFinalization();
 
+        new Tank().changeState(true);
+        System.gc();
+        System.runFinalization();
     }
 }
 
-class FinalizeTry{
+/*class FinalizeTry{
     protected void finalize(){
-        System.out.println();
+        System.out.println("123123");
+    }
+}*/
+
+class Tank{
+    boolean state = false;
+    void changeState(boolean state){
+        this.state = state;
+    }
+    public void finalize() throws Throwable{
+        if (state){
+            System.out.println("满的。。。有问题");
+        }else {
+            System.out.println("空的。。。没问题");
+        }
     }
 }
